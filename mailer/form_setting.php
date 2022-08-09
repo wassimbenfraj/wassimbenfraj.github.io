7d326ec0986bc92a75507551481cb8fd4e2cd2c8
wassimbenfraj <wassim.benfraj@esprit.tn>
2022-08-09 15:31:25 +0100

    testing contact

--- mailer/form_setting.php
+++
@@ -1,8 +0,0 @@
-<?php
-	/*Form settings*/
-	$subj = "New message from the site 'Wassim Benfraj Website'"; //letter subject
-	$to = 'wassim.benfraj@esprit.tn'; // Enter Your E-mail
-	$from = 'w3ssim220@gmail.com'; // Admin e-mail
-	$fromName = 'Your Company Name'; // Your company name
-	$charset = 'UTF-8';
-?>
