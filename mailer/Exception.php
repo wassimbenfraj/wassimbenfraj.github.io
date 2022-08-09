5b25fb93d208fcce0ca32d65b52f998a6a105ed9
wassimbenfraj <wassim.benfraj@esprit.tn>
2022-08-09 15:22:28 +0100

    testing contact

---
+++ mailer/Exception.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace PHPMailer\PHPMailer;
+
+
+class Exception extends \Exception
+{
+    /**
+     * Prettify error message output.
+     *
+     * @return string
+     */
+    public function errorMessage()
+    {
+        return '<strong>' . htmlspecialchars($this->getMessage()) . "</strong><br />\n";
+    }
+}
---
+++ mailer/form_setting.php
@@ -0,0 +1,8 @@
+<?php
+	/*Form settings*/
+	$subj = "New message from the site 'Wassim Benfraj Website'"; //letter subject
+	$to = 'wassim.benfraj@esprit.tn'; // Enter Your E-mail
+	$from = 'w3ssim220@gmail.com'; // Admin e-mail
+	$fromName = 'Your Company Name'; // Your company name
+	$charset = 'UTF-8';
+?>
